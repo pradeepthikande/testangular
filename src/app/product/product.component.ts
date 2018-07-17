0b1cc73039e69022f286053374a27fb820e6f3e0
pradeepthikande <39901395+pradeepthikande@users.noreply.github.com>
2018-07-17 19:30:10 +0530

    Update product.component.ts
    product update

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FactoryService } from '../../app/factory.service';
 import { Product } from '../../app/app.interface';
 @Component({
@@ -7,16 +7,28 @@ import { Product } from '../../app/app.interface';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  pro:Product [];
+  visible = false;
+
+  p1: Product;
+
+ show(i){
+  this.visible = true;
+   this.p1=this.pro[i];
+   console.log(this.p1);
+ }
+  pro:Product[];
   constructor(private produce: FactoryService) { }
 
   ngOnInit() {
     this.loadProduct();
+    // this.show(this.get,this.i);
   }
   loadProduct(){
-    this.produce. getProduct().subscribe(data=>{
+    
+    this.produce.getProduct().subscribe(data=>{
       console.log(data);
-      this.pro=data.product
+      this.pro=data.product;
+      console.log(this.pro);
     });
   }
 
