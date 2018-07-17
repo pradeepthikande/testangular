6c32aff205596cf23a3307ad027556f46f6e4b5a
pradeepthikande <39901395+pradeepthikande@users.noreply.github.com>
2018-07-17 19:28:09 +0530

    Update form.component.ts
    form updated

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewEncapsulation ,Input} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { Product } from '../../app/app.interface';
 @Component({
   selector: 'app-form',
@@ -7,22 +7,35 @@ import { Product } from '../../app/app.interface';
   encapsulation: ViewEncapsulation.None
 })
 export class FormComponent implements OnInit {
-  title:string="NEW PRODUCT";
+  title: string = "NEW PRODUCT";
+
+  @Input() sending: Product;
   @Input() details: Product;
-  constructor() { }
+  // @Input() details: Product = {} as Product;
+  constructor() {
+        setTimeout(()=>{
+    this.details=this.sending;
+
+
+        },1000)
+
+
+  }
 
   ngOnInit() {
     if(this.details===undefined){
-      this.details= {
-    productName: '',
+    this.details= {
+    name: '',
     description: '',
-    md: new Date,
-    ed: new Date
+    md: new Date(),
+    ed: new Date()
     }
   }
+  }
 
+  add() {
+    // console.log(JSON.stringify(this.details));
+    console.log(this.details);
+  }
 }
-add(){
-  console.log(JSON.stringify(this.details));
-}
-}
+
